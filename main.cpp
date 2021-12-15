a899e67e4a766c8cde32a14464e186d1bf532b0d
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2021-12-15 11:23:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,39 @@
-
+#include <iostream> 
+#include <random>
+#include <chrono>
+#include <fstream>
+#include <sstream> 
+#include <vector>
+using namespace std;
+int main(){
+	float one, two;
+  int length;
+  double* pint;
+  length = 0;
+  pint = nullptr;
+  ifstream plik;
+  plik.open("Values.txt");
+  vector<pair<float, float>> w;
+  do {
+	  plik >> one;
+	  plik >> two;
+	  w.push_back({ one, two });
+  } while (!plik.eof());
+  length=w.size();
+  cout << length;
+  ofstream plikprim;
+  plikprim.open("Results.txt");
+  for (int i = 0; i < length; i++) {
+	  if (w[i].first > 190.0) {
+		  plikprim << "basketball"<<endl;
+	  }
+	  else{
+		  if (w[i].second > 50) {
+			  plikprim << "basketball" << endl;
+		  }
+		  else {
+			  plikprim << "athletics" << endl;
+		  }
+	  }
+  }
+}
