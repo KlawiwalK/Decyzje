367e5b23dda97d6c1239c89c8214daeb5b91de70
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-01-24 17:54:28 +0100

    Update Function.h

--- Function.h
+++ Function.h
@@ -1,18 +1,16 @@
 #pragma once
 #include <iostream> 
-#include <random>
-#include <chrono>
 #include <fstream>
 #include <sstream> 
 #include <vector>
 #include <string>
+using namespace std;
 
 
-vector<pair<float, float>> Openvalues(string onen, string twot, int length, int one, int two);
+vector<pair<float, float>> Openvalues(int length);
 
-vector<string> Setrules(string rule, string znak, string znakprim, float value, float valueprime, string wynik1, string wynik2, string wynik3);
+void Setrule(string znak, string znakprim, float value, float valueprime, string wynik1, string wynik2, string wynik3);
 
-vector<vector<pair<float, float>>> Variables(string znak, string znakprim, int length, float value, float valueprime, string wynik1, string wynik2, string wynik3);
+void Variables(vector<pair<float, float>> w, string znak, string znakprim, int length, float value, float valueprime, string wynik1, string wynik2, string wynik3);
 
-void Dividing(vector<pair<float, float>>firstresult, vector<pair<float, float>>secondresult, string wynik1, string wynik3);
 
