b49107f7476e768245485623817ca37dacbb193c
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-01-24 16:21:31 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <random>
+#include <chrono>
+#include <fstream>
+#include <sstream> 
+#include <vector>
+
+void Openvalues();
+
+void Setrules();
+
+void Variables();
+
+void Dividing();
