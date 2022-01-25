e399f4f3433fe5ebb595d19e1986f92b6094ac28
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-01-25 17:44:54 +0100

    Update Function.h

--- Function.h
+++ Function.h
@@ -7,10 +7,8 @@
 using namespace std;
 
 
-vector<pair<float, float>> Openvalues(int length);
+vector<pair<float, float>> Openvalues(int& length);
 
-void Setrule(string znak, string znakprim, float value, float valueprime, string wynik1, string wynik2, string wynik3);
+void Setrule(string& znak, string& znakprim, float& value, float& valueprime, string& wynik1, string& wynik2, string& wynik3);
 
 void Variables(vector<pair<float, float>> w, string znak, string znakprim, int length, float value, float valueprime, string wynik1, string wynik2, string wynik3);
-
-
