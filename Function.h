7b79bd4b9295479febb40890185f8a6bd0c1ea78
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-01-27 17:26:38 +0100

    Update Function.h

--- Function.h
+++ Function.h
@@ -6,15 +6,14 @@
 #include <string>
 using namespace std;
 
-
-vector<pair<float, float>> Openvalues(int& length);
 /**Funkcja pobiera zawartość pliku "wartości", a następnie zczytuje je do wektora par.
 Pierwsze dwa elementy są usuwane, albowiem zawierają one wartość typu string, która opisuje zmienną.
 Pozostałe elementy są zczytywane do końca pliku, a następnie przekazywane dalej w formie wektora.
 @param length określa długość zwracanego wektora.
 @return vector<pair<float, float> w **/
 
-void Setrule(string& znak, string& znakprim, string& test, float& value, float& valueprime, string& wynik1, string& wynik2, string& wynik3);
+vector<pair<float, float>> Openvalues(int& length);
+
 /**Drzewko zasad jest drzewem binarnym, co oznacza, iż w przypadku spełnienia, bądź niespełnienia warunku 0,
 może zostać wykonany warunek 1. Setrule rozdziela poszczególne elementy drzewka zasad na pojedyncze elementy,
 a następnie przekazuje je dalej w formie ustalonych zmiennych.
@@ -28,7 +27,9 @@ a następnie przekazuje je dalej w formie ustalonych zmiennych.
 @param wynik3 jest rezultatem niespełnienia warunku "1".
 @return 0 **/
 
-void Variables(vector<pair<float, float>> w, string znak, string test, string znakprim, int length, float value, float valueprime, string wynik1, string wynik2, string wynik3);
+void Setrule(string& znak, string& znakprim, string& test, float& value, float& valueprime, string& wynik1, string& wynik2, string& wynik3);
+
+
 /** Funkcja sprawdza, który warunek 0 musi zostać spełniony, żeby można było przejść do warunku 1, a następnie 
 sprawdza, które warunki zostały spełnione, w zależności od podanych paramertów oraz znaków mniejszości, równości lub
 większości.
@@ -43,4 +44,7 @@ większości.
 @param wynik2 jest rezultatem spełnienia warunku "1".
 @param wynik3 jest rezultatem niespełnienia warunku "1".
 @return 0 **/
+
+void Variables(vector<pair<float, float>> w, string znak, string test, string znakprim, int length, float value, float valueprime, string wynik1, string wynik2, string wynik3);
+
 
