23dafdaac01a1405ae26bc51496a61ddbf07ff07
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-01-27 14:48:52 +0100

    Update Function.h

--- Function.h
+++ Function.h
@@ -10,16 +10,37 @@ using namespace std;
 vector<pair<float, float>> Openvalues(int& length);
 /**Funkcja pobiera zawartość pliku "wartości", a następnie zczytuje je do wektora par.
 Pierwsze dwa elementy są usuwane, albowiem zawierają one wartość typu string, która opisuje zmienną.
-Pozostałe elementy są zczytywane do końca pliku, a następnie przekazywane dalej w formie wektora.**/
+Pozostałe elementy są zczytywane do końca pliku, a następnie przekazywane dalej w formie wektora.
+@param length określa długość zwracanego wektora.
+@return vector<pair<float, float> w **/
 
 void Setrule(string& znak, string& znakprim, string& test, float& value, float& valueprime, string& wynik1, string& wynik2, string& wynik3);
 /**Drzewko zasad jest drzewem binarnym, co oznacza, iż w przypadku spełnienia, bądź niespełnienia warunku 0,
 może zostać wykonany warunek 1. Setrule rozdziela poszczególne elementy drzewka zasad na pojedyncze elementy,
-a następnie przekazuje je dalej w formie ustalonych zmiennych.**/
+a następnie przekazuje je dalej w formie ustalonych zmiennych.
+@param znak określa rodzaj znaku dla warunku "0" (Mniejszości, większości lub równości).
+@param znakprim określa rodzaj znaku dla warunku "1" (Mniejszości, większości lub równości).
+@param test jest rezultatem niespełnienia warunku "0".
+@param value jest wartością, do której porównane zostaną parametry na pierwszym miejscu z wektora w.
+@param valueprime jest wartością, do której porównane zostaną parametry na drugim miejscu z wektora w.
+@param wynik1 jest rezultatem spełnienia warunku "0".
+@param wynik2 jest rezultatem spełnienia warunku "1".
+@param wynik3 jest rezultatem niespełnienia warunku "1".
+@return 0 **/
 
 void Variables(vector<pair<float, float>> w, string znak, string test, string znakprim, int length, float value, float valueprime, string wynik1, string wynik2, string wynik3);
 /** Funkcja sprawdza, który warunek 0 musi zostać spełniony, żeby można było przejść do warunku 1, a następnie 
 sprawdza, które warunki zostały spełnione, w zależności od podanych paramertów oraz znaków mniejszości, równości lub
-większości.**/
-
+większości.
+@param vector<pair<float, float>> w jest wektorem zawierającym wartości z pliku "Values".
+@param znak określa rodzaj znaku dla warunku "0" (Mniejszości, większości lub równości).
+@param znakprim określa rodzaj znaku dla warunku "1" (Mniejszości, większości lub równości).
+@param length określa długość zwracanego wektora.
+@param test jest rezultatem niespełnienia warunku "0".
+@param value jest wartością, do której porównane zostaną parametry na pierwszym miejscu z wektora w.
+@param valueprime jest wartością, do której porównane zostaną parametry na drugim miejscu z wektora w.
+@param wynik1 jest rezultatem spełnienia warunku "0".
+@param wynik2 jest rezultatem spełnienia warunku "1".
+@param wynik3 jest rezultatem niespełnienia warunku "1".
+@return 0 **/
 
