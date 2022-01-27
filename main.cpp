e5547c36d6199d4d58e2dbd8ee776064c5c31835
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-01-27 13:34:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <fstream>
 #include <sstream> 
 #include <vector>
-#include "Function.h"
+#include "FileModel.h"
 using namespace std;
 int main() {
-	float value, valueprime, one, two;
-	int length, lengthprim, lengthresult1, lengthresult2;
-	string znak, znakprim, rule, result1, result2, wynik1, wynik2, wynik3;
+	float value, valueprime;
+	int length, lengthresult1, truth;
+	string znak, test, znakprim, rule, result1, result2, wynik1, wynik2, wynik3;
 	length = 0;
 	value = 0;
 	valueprime = 0;
@@ -18,11 +18,10 @@ int main() {
 	vector<pair<float, float>> secondresult;
 	w=Openvalues(length);
 
-	Setrule(znak, znakprim, value, valueprime, wynik1,  wynik2, wynik3);
+	Setrule(znak, znakprim, test, value, valueprime, wynik1,  wynik2, wynik3);
 	
 
-	Variables(w,znak, znakprim, length,value,  valueprime, wynik1, wynik2,  wynik3);
+	Variables(w, znak, test, znakprim, length, value, valueprime, wynik1, wynik2, wynik3);
 
 	return 0;
 }
-
