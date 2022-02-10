6f4bfd5a6459ff11ed4a30d7d01393034f896dac
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-02-10 11:42:06 +0100

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -3,270 +3,125 @@
 #include <sstream> 
 #include <vector>
 #include "Function.h"
+#include <map>
 using namespace std;
-vector<pair<float, float>> Openvalues(int& length) {
+map<string, vector<double>> Openvalues(int& e, int& mapl, vector<string>& line) {
+	map<string, vector<double>> values;
 	ifstream plik;
-	float one, two;
-	string onen, twot;
-	plik.open("Values.txt");
+	double one;
+	string str, onen;
+	plik.open(Values);
+
 	if (plik.is_open()) {
-		if (plik >> onen >> twot) {
-			vector<pair<float, float>> w;
+		mapl = 0;
+		if (getline(plik, str)) {
+			stringstream decoder;
+			decoder << str;
+			string s;
+			while (decoder >> s) {
+				line.push_back(s);
+			}
+			vector<double> w;
+			e = 0;
 			do {
-				plik >> one;
-				plik >> two;
-				w.push_back({ one, two });
+				double tempdb;
+				for (auto label : line) {
+					plik >> tempdb;
+					values[label].push_back(tempdb);
+				}
+				e++;
 			} while (!plik.eof());
-			length = w.size();
-			length = length - 1;
-			cout << length;
-			return w;
+			mapl = values.size();
+			return values;
 		}
 		else {
-			cout << "If you want to use this properly, then you should put"
-				"some values in the values.txt in pairs, which will allows the program to compare them.";
-			length = 0;
-			vector<pair<float, float>> empty = {};
-			return empty;
+			cout << " In order to use this program properly, in file 'Values' you have to put at least two" << endl <<
+				"different values, which will be compared. Each value should be labelled in the first line" << endl <<
+				" the same way as in 'Rules' file. ";
+			do {
+				double tempdb;
+				for (auto label : line) {
+					plik >> tempdb;
+					values[label].push_back(tempdb);
+				}
+				e++;
+			} while (!plik.eof());
+			mapl = 0;
+			return values;
 		}
 	}
 }
 
-void Setrule(string& znak, string& znakprim, string& test, float& value, float& valueprime, string& wynik1, string& wynik2, string& wynik3) {
+
+map<string, Rule> Setrule() {
 	ifstream plikdwa;
 	string rule;
 	plikdwa.open("Rules.txt");
-	vector<string> p;
-	do {
-		plikdwa >> rule;
-		p.push_back({ rule }); // Reads data from rules.txt to a vector of strings.
-	} while (!plikdwa.eof());
+	map<string, Rule> p;
+	bool greater;
+	double compare;
+	string key, value, positive, negative;
+	char sign;
 
-	znak = p[2]; //Types the equality or inequality mark from the first condition into a "znak" variable
-	znakprim = p[8]; //Types the equality or inequality mark from the second rule condition a "znakprim" variable
-	value = stof(p[3]); //Changes the value from the first rule condition a float and puts it as a "value" variable
-	valueprime = stof(p[9]); //Changes the value from the second condition into a float and puts it as a "valueprime" variable
-	test = p[4];
-	wynik1 = p[5]; // Positive result of the first condition, changed into a slightly more friendly for eyes variable.
-	wynik2 = p[11]; // Positive result of the second condition, changed into a slightly more friendly for eyes variable, just like before.
-	wynik3 = p[10]; // The same as the one above, but a negative result instead of a positive one.
+	while (plikdwa >> key >> value >> sign >> compare >> positive >> negative) {
+		if (sign == '>')greater = true;
+		else greater = false;
+		p[key] = { greater, compare, value, positive, negative };
+	}
+	return p;
 }
 
 
-
-
-
-void Variables(vector<pair<float, float>> w, string znak, string test, string znakprim, int length, float value, float valueprime, string wynik1, string wynik2, string wynik3) {
-	ofstream plikprim;
-	plikprim.open("Results.txt");
-	vector<pair<float, float>> firstresult;
-	vector<pair<float, float>> secondresult;
-	vector<pair<float, float>> thirdresult;
-	if (test == "1") { //Checks, if the function has to jump in case of a negative or positive solution of the first rule
-		if (znak == ">") { //If mark from the first condition is this one, checks if it is true.
-			for (int j = 0; j < length; j++) {
-				if (w[j].first > value) {
-					firstresult.push_back({ w[j].first, w[j].second });
-					cout << wynik1 << endl;
-				}
-				else { //If not true, checks the inequality or equality mark in the second condition.
-					if (znakprim == ">") {
-						if (w[j].second > valueprime) {
-							thirdresult.push_back({ w[j].first, w[j].second });
-							cout << wynik2 << endl;
-						}
-						else {
-							secondresult.push_back({ w[j].first, w[j].second });
-							cout << wynik3 << endl;
-						}
-					}
-					if (znakprim == "<") {
-						if (w[j].second < valueprime) {
-							thirdresult.push_back({ w[j].first, w[j].second });
-							cout << wynik2 << endl;
-						}
-						else {
-							cout << wynik3 << endl;
-							secondresult.push_back({ w[j].first, w[j].second });
-						}
-					}
-				}
-
+string Applyrule(map<string, Rule>& rules, map<string, vector<double>>& values, string ruleNumber, const int i) {
+	cout << ruleNumber << i << endl;
+	if (rules.find(ruleNumber) != rules.end()) {
+		cout << "try";
+		Rule r = rules[ruleNumber];
+		cout << endl << r.greater << " " << r.compare << " " << r.value << " " << r.positive << " " << r.negative;
+		if (r.greater) {
+			cout << endl << values[r.value][0] << " " << values[r.value][i];
+			if (values[r.value][i] < r.compare) {
+				cout << "1";
+				return Applyrule(rules, values, r.positive, i);
 			}
-		}
-		if (znak == "<") {
-			for (int j = 0; j < length; j++) {
-
-				if (w[j].first < value) {
-					firstresult.push_back({ w[j].first, w[j].second });
-					cout << wynik1 << endl;
-				}
-				else {
-					if (znakprim == "=") {
-						if (w[j].second == valueprime) {
-							thirdresult.push_back({ w[j].first, w[j].second });
-							cout << wynik2 << endl;
-						}
-						else {
-							cout << wynik3 << endl;
-							secondresult.push_back({ w[j].first, w[j].second });
-						}
-					}
-					if (znakprim == "<") {
-						if (w[j].second < valueprime) {
-							thirdresult.push_back({ w[j].first, w[j].second });
-							cout << wynik2 << endl;
-						}
-						else {
-							cout << wynik3 << endl;
-							secondresult.push_back({ w[j].first, w[j].second });
-						}
-
-					}
-				}
-
-			}
-		}
-		int lengthresult1 = firstresult.size();
-		int lengthresult2 = secondresult.size();
-		int lengthresult3 = thirdresult.size();
-		plikprim << wynik1 << endl;
-		for (int i = 0; i < lengthresult1; i++) {
-			plikprim << firstresult[i].first << " " << firstresult[i].second << endl;
-		}
-		plikprim << wynik3 << endl;
-		for (int i = 0; i < lengthresult2; i++) {
-			plikprim << secondresult[i].first << " " << secondresult[i].second << endl;
-
-		}
-		plikprim << wynik2 << endl;
-		for (int i = 0; i < lengthresult3; i++) {
-			plikprim << thirdresult[i].first << " " << thirdresult[i].second << endl;
-
-		}
-	}
-	else {
-		if (wynik1 == "1") {
-			if (znak == ">") {
-				for (int j = 0; j < length; j++) {
-					if (w[j].first < value or w[j].first == value) {
-						firstresult.push_back({ w[j].first, w[j].second });
-						cout << test << endl;
-					}
-					else {
-						if (znakprim == ">") {
-							if (w[j].second > valueprime) {
-								thirdresult.push_back({ w[j].first, w[j].second });
-								cout << wynik2 << endl;
-							}
-							else {
-								secondresult.push_back({ w[j].first, w[j].second });
-								cout << wynik3 << endl;
-							}
-						}
-						if (znakprim == "<") {
-							if (w[j].second < valueprime) {
-								thirdresult.push_back({ w[j].first, w[j].second });
-								cout << wynik2 << endl;
-							}
-							else {
-								cout << wynik3 << endl;
-								secondresult.push_back({ w[j].first, w[j].second });
-							}
-						}
-					}
-
-				}
-			}
-			if (znak == "<") {
-				for (int j = 0; j < length; j++) {
-
-					if (w[j].first > value or w[j].first == value) {
-						firstresult.push_back({ w[j].first, w[j].second });
-						cout << test << endl;
-					}
-					else {
-						if (znakprim == ">") {
-							if (w[j].second > valueprime) {
-								thirdresult.push_back({ w[j].first, w[j].second });
-								cout << wynik2 << endl;
-							}
-							else {
-								cout << wynik3 << endl;
-								secondresult.push_back({ w[j].first, w[j].second });
-							}
-						}
-						if (znakprim == "<") {
-							if (w[j].second < valueprime) {
-								thirdresult.push_back({ w[j].first, w[j].second });
-								cout << wynik2 << endl;
-							}
-							else {
-								cout << wynik3 << endl;
-								secondresult.push_back({ w[j].first, w[j].second });
-							}
-
-						}
-					}
-
-				}
-			}
-			int lengthresult1 = firstresult.size();
-			int lengthresult2 = secondresult.size();
-			int lengthresult3 = thirdresult.size();
-			plikprim << test << endl;
-
-			for (int i = 0; i < lengthresult1; i++) {
-				plikprim << firstresult[i].first << " " << firstresult[i].second << endl;
-			}
-			plikprim << wynik3 << endl;
-			for (int i = 0; i < lengthresult2; i++) {
-				plikprim << secondresult[i].first << " " << secondresult[i].second << endl;
-
-			}
-			plikprim << wynik2 << endl;
-			for (int i = 0; i < lengthresult3; i++) {
-				plikprim << thirdresult[i].first << " " << thirdresult[i].second << endl;
+			else {
+				cout << "2";
+				return Applyrule(rules, values, r.negative, i);
 			}
 		}
 		else {
-			if (znak == ">") {
-				for (int j = 0; j < length; j++) {
-					if (w[j].first < value or w[j].first == value) {
-						firstresult.push_back({ w[j].first, w[j].second });
-						cout << test << endl;
-					}
-					else {
-						secondresult.push_back({ w[j].first, w[j].second });
-						cout << wynik1 << endl;
-					}
-				}
-			}
-			if (znak == "<") {
-				for (int j = 0; j < length; j++) {
-					if (w[j].first > value or w[j].first == value) {
-						firstresult.push_back({ w[j].first, w[j].second });
-						cout << test << endl;
-					}
-					else {
-						secondresult.push_back({ w[j].first, w[j].second });
-						cout << wynik1 << endl;
-					}
-				}
-			}
-		}
-		int lengthresult1 = firstresult.size();
-		int lengthresult2 = secondresult.size();
-		plikprim << test << endl;
 
-		for (int i = 0; i < lengthresult1; i++) {
-			plikprim << firstresult[i].first << " " << firstresult[i].second << endl;
-		}
-		plikprim << wynik3 << endl;
-		for (int i = 0; i < lengthresult2; i++) {
-			plikprim << secondresult[i].first << " " << secondresult[i].second << endl;
-
+			if (values[r.value][i] > r.compare) {
+				return Applyrule(rules, values, r.positive, i);
+			}
+			else {
+				return Applyrule(rules, values, r.negative, i);
+			}
 		}
 	}
-			
+	else {
+		cout << "tryno";
+		return ruleNumber;
+	}
+}
+
+
+void Variables(map<string, vector<double>> values, map<string, Rule>& rules, int e,  vector<string>& line) {
+	map<string, vector<int>> results;
+	ofstream plik;
+	plik.open("Results.txt");
+	cout << e;
+	for (int i = 0; i < e; i++) {
+		results[Applyrule(rules, values, "0", i)].push_back(i);
+	}
+	cout << "testapp";
+	for (auto res : results) {
+		plik << res.first << endl;
+		for (auto k : res.second) {
+			for (auto name : line) {
+				plik << values[name][k];
+			}
+			plik << endl;
+		}
+	}
 }
