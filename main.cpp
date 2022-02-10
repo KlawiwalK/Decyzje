38cb7a136752f146f1e1a07aca49472b76c383ac
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-02-10 11:43:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,27 +2,38 @@
 #include <fstream>
 #include <sstream> 
 #include <vector>
-#include "Function.h"
+#include <map>
+#include "FileModel.h"
 using namespace std;
-int main() {
-	float value, valueprime;
-	int length, lengthresult1, truth;
-	string znak, test, znakprim, rule, result1, result2, wynik1, wynik2, wynik3;
-	length = 0;
+int main(int argc, char* argv[]) {     
+	// -i -o -p
+	if (argc != 3) {
+		return 0; // Jeśli nie podadzą dwóch plików, cała funkcja się kończy, zanim się zacznie
+	}
+	map<string, vector<double>> values;
+	map<string, Rule> rules;
+	double value;
+	int e, mapl; //mapl sprawdza, czy plik Values nie jest pusty
+	string Values, Rules, Results, filename1, filename2, filename3, testow, test, wynik;
 	value = 0;
-	valueprime = 0;
-	vector<pair<float, float>> w;
-	vector<string> p;
+	vector<string> line;
+	Values = argv[0];
+	Rules = argv[1];
+	Results = argv[2];
+	values=Openvalues( e, mapl, line);
+	cout << "test 1";
+	if (mapl == 0) {
+		ofstream plik;
+		plik.open("Results.txt");
+		plik << "Wrong data";
+		return 0;
+	}
 
-	vector<pair<float, float>> firstresult;
-	vector<pair<float, float>> secondresult;
-	vector<pair<float, float>> thirdresult;
-	w=Openvalues(length);
+	rules=Setrule();
+	cout << "test2";
 
-	Setrule(znak, znakprim, test, value, valueprime, wynik1,  wynik2, wynik3);
-	
-
-	Variables(w, znak, test, znakprim, length, value, valueprime, wynik1, wynik2, wynik3);
+	Variables(values,rules,e, line);
+	cout << "test3";
 
 	return 0;
 }
