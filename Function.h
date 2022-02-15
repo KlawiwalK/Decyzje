d58ed2740ea119e94636f8b0f228ed0c0282e2ba
KlawiwalK <92019502+KlawiwalK@users.noreply.github.com>
2022-02-15 18:26:33 +0100

    Update Function.h

--- Function.h
+++ Function.h
@@ -13,19 +13,22 @@ struct Rule {
     string positive;
     string negative;
 };
-
+/**Function opens the file and checks, if it is not empty.
+In case of an empty file, function tells the user how 
+to use the program and returns "Wrong data" into results file. 
+If the file is not empty, function takes the first 
+line of the program and saves the rest of the file 
+to a map of values.**/
 map<string, vector<double>> Openvalues(int& e, int& mapl, string& Values, vector<string>& line);
-/**Funkcja otwiera plik z wartościami podany przez użytkownika, a następnie sprawdza, czy podany plik nie jest pusty. 
-W przypadku pustego pliku, funkcja zwraca zmienną "mapl" równą zero, informującą o pustym pliku, a także wypisuje krótką informację o tym, jak należy użyć programu. 
-W przeciwnym wypadku funkcja pobiera pierwszą linię pliku, a pozostałe wartości zapisuje do mapy "values".**/
 
+/**Funckion uses a structure, whici values saves as a map.
+It reads the binary tree and allows to analyse the data in it.**/ 
 map<string, Rule> Setrule(string& Rules);
-/**Funckja korzysta ze struktury, której wartości zapisuje do mapy. Służy ona do odczytania drzewa binarnego i analizy danych w nim zawartych.**/
 
+/** Function compares values from the values file and the binary tree, and then
+sends its result. Works together with function Variables.**/
 string Applyrule(map<string, Rule>& rules, map<string, vector<double>>& values, string ruleNumber, const int i);
-/**Funckja porównuje ze sobą wartości z pliku wartości oraz z drzewa binarnego, a następnie zwraca wartość zależną od otrzymanego wyniku.
-Działa ona razem z funkcją Variables.**/
 
+
+/** Function Variables uses the function Applyrule, and then saves the result to a file.**/
 void Variables(string& Results, map<string, vector<double>> values, map<string, Rule>& rules, int e, vector<string>& line);
-/**Funckja Variables wykonuje funkcję Applyrule dla wszystkich grup z pliku wartości, a następnie w zależności od wyniku funckji Applyrule, 
-zapisuje odpowiednią wartość do pliku.**/
